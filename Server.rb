11193b934b33a7d28efde5f00132c37e69af9650
Tara Matthews <matthet@tcd.ie>
2014-10-11 18:01:09 +0100

    sure that's a godd bit..

--- Server.rb
+++ Server.rb
@@ -1,8 +1,54 @@
+require 'thread'
 require "socket"
-server = TCPServer.open(2626)
+
+class Pool
+
+  def initialize(size)
+    @size = size
+    @jobs = Queue.new
+    
+    @pool = Array.new(@size) do |i|
+      Thread.new do
+        Thread.current[:id] = i
+
+        catch(:exit) do
+          loop do
+            job, args = @jobs.pop
+            job.call(*args)
+          end
+        end
+      end
+    end
+  end
+  
+  # ### Work scheduling  
+  # To schedule a piece of work to be done is to say to the `Pool` that you
+  # want something done.
+  def schedule(*args, &block)
+    @jobs << [block, args]
+  end
+  
+  def shutdown
+    @size.times do
+      schedule { throw :exit }
+    end
+    @pool.map(&:join)
+  end
+end
+
+# Demonstration
+# -------------
+# Running this file will display how the thread pool works.
+if $0 == __FILE__
+    server = TCPServer.open(2626)
+    p = Pool.new(10)
+
 loop do
-    Thread.fork(server.accept) do |client| 
-        client.puts("Hello", "Goodbye xo")
+    p.schedule(server.accept) do |client|
+        client.puts("Job #{i} finished by thread #{Thread.current[:id]}")
         client.close
     end
+end
+
+  at_exit { p.shutdown }
 end
