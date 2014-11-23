59eb4bc13f0a5d41714346f0b1992818b83d105f
Tara Matthews <matthet@tcd.ie>
2014-11-23 00:14:43 +0000

    Client code: concurrent listening and sending, Server code: client identification

--- Server.rb
+++ Server.rb
@@ -1,14 +1,23 @@
 require 'thread'
 require "socket"
 
-class Pool
+class Server
+  def initialize(size, ip, port)
+    @port = port
+    @ip = ip
+    @server = TCPServer.new(@ip, @port)
+    @connections = Hash.new
+    @rooms = Hash.new
+    @clients = Hash.new
+    @connections[:server] = @server
+    @connections[:rooms] = @rooms
+    @connections[:clients] = @clients
 
-  def initialize(size)
     @size = size
     @jobs = Queue.new
 
-    # Each thread store its’ index in a thread-local variable, in case we
-    # need to know which thread a job is executing in later on.  
+    # Threadpooled Multithreaded Server to handle Client requests 
+    # Each thread store its’ index in a thread-local variable  
     @pool = Array.new(@size) do |i|
       Thread.new do
         Thread.current[:id] = i
@@ -22,11 +31,42 @@ class Pool
         end
       end
     end
+  run
   end
   
-  # Tell the Pool that there is work to be done. 
+  def run
+    loop do
+      schedule(@server.accept) do |client|
+        client.puts("Connected to Chat Server!\nLogin (HELO text):")
+        message = client.gets
+        client.puts("#{message}IP:#{@ip}\nPort:#{@port}\nStudentID:11374331\n")
+	nick_name = client.gets.chomp.to_sym
+        @connections[:clients].each do |other_name, other_client|
+          if nick_name == other_name || client == other_client
+            client.puts "This username already exist"
+            client.close
+          end
+        end
+      end
+    end
+  end
+
   def schedule(*args, &block)
     @jobs << [block, args]
+  end
+
+  def listen_client(client)
+    loop do
+      message = client.gets
+      if message[0..3] == "HELO"
+        client.puts"#{message}IP:#{@ip}\nPort:#{@port}\nStudentID:11374331"
+      elsif message == "KILL_SERVICE\n"
+        client.puts("Service Killed")
+        @server.close
+      else
+        client.puts("Aw you put your own message! I'm just going to say Hey! Bye..")
+      end
+    end
   end
   
   def shutdown
@@ -37,30 +77,6 @@ class Pool
   end
 end
 
-# Start Server, Receive Client Message, Process, Reply to Client.
-if $0 == __FILE__
-  port = 2631
-  host_ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
-
-  server = TCPServer.new(host_ip, port)
-  p = Pool.new(10)
-
-  loop do
-    p.schedule(server.accept) do |client|
-      message = client.gets
-      if message[0..3] == "HELO"
-          client.puts"#{message}IP:#{host_ip}\nPort:#{port}\nStudentID:11374331"
-          client.close
-        elsif message == "KILL_SERVICE\n"
-          client.puts("Service Killed")
-          client.close
-	  server.close
-        else
-          client.puts("Aw you put your own message! I'm just going to say Hey! Bye..")
-          client.close
-      end
-    end
-  end
-  server.close
-  at_exit { p.shutdown }
-end
+port = 2631
+ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
+Server.new(10, ip, port)
